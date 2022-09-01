37f7d3f7206f58b00c73328f76e19937d29315a8
Martin Craig <martin.craig@nottingham.ac.uk>
2022-09-01 13:48:23 +0100

    WIP: writing out commands correctish

--- hcp_notts/main.py
+++ hcp_notts/main.py
@@ -7,11 +7,8 @@ Command line interface
 import os
 import argparse
 import sys
-import logging
 
 from ._version import __version__
-
-LOG = logging.getLogger(__name__)
 
 def makedirs(dpath, exist_ok=False):
     """
@@ -24,34 +21,12 @@ def makedirs(dpath, exist_ok=False):
         if not exist_ok or exc.errno != errno.EEXIST:
             raise
 
-def setup_logging(logfile=None, log_level="info", log_stream=None):
-    """
-    Set the log level, formatters and output streams for the logging output
-
-    By default this goes to <outdir>/logfile at level INFO
-    """
-
-    # Set log level on the root logger to allow for the possibility of 
-    # debug logging on individual loggers
-    level = getattr(logging, level.upper(), logging.INFO)
-    logging.getLogger().setLevel(level)
-
-    if logfile:
-        # Send the log to an output logfile
-        makedirs(os.path.dirname(logfile), True)
-        logging.basicConfig(filename=logfile, filemode="w", level=level)
-
-    if log_stream:
-        # Can also supply a stream to send log output to as well (e.g. sys.stdout)
-        extra_handler = logging.StreamHandler(log_stream)
-        extra_handler.setFormatter(logging.Formatter('%(levelname)s : %(message)s'))
-        logging.getLogger().addHandler(extra_handler)
-
 def envdir(var):
     dpath = os.environ.get(var, "")
     if not dpath or not os.path.isdir(dpath):
-        sys.stderr.write("{var} not set or is not a directory")
+        sys.stderr.write(f"{var} not set or is not a directory\n")
         sys.exit(1)
+    return dpath
 
 def main():
     parser = argparse.ArgumentParser(f'HCP pipelines Nottingham helper script v{__version__}', add_help=True)
@@ -61,9 +36,10 @@ def main():
     g.add_argument('--t1w', help='Name of T1w file within subject subfolder. May contain {subjid} placeholder', default="T1w")
     g.add_argument('--t2w', help='Name of T2w file within subject subfolder, if available. May contain {subjid} placeholder')
     g.add_argument('--avgrdc-method', help='Averaging and readout distortion correction methods', default="NONE", choices=["NONE", "TOPUP", "FIELDMAP"])
-    g.add_argument('--skip', nargs="*", help='Skip section of pipeline', choices=["PreFreeSurfer", "FreeSurfer", "PostFreeSurfer"])
+    g.add_argument('--skip', nargs="*", help='Skip section of pipeline', choices=["PreFreeSurfer", "FreeSurfer", "PostFreeSurfer"], default=[])
     g.add_argument('--logfile', help='File to log script output')
-    g.add_argument('--logdir', help='Name of subfolder for subject logs')
+    g.add_argument('--logdir', help='Name of subfolder for subject logs', default='hcp_pipeline_logs')
+    g.add_argument('--debug', help='Enable debug logging', action="store_true", default=False)
 
     g = parser.add_argument_group("Fieldmap options")
     g.add_argument('--fmap-mag', help='Fieldmap magnitude file name', default="FieldMap_Magnitude.nii.gz")
@@ -87,10 +63,10 @@ def main():
 
     g = parser.add_argument_group("Templates - non-absolute paths are relative to HCP template dir")
     g.add_argument('--template-t1w', help='T1w template', default="MNI152_T1_0.7mm.nii.gz")
-    g.add_argument('--template-t1w-template-brain', help='T1w brain template', default="MNI152_T1_0.7mm_brain.nii.gz")
+    g.add_argument('--template-t1w-brain', help='T1w brain template', default="MNI152_T1_0.7mm_brain.nii.gz")
     g.add_argument('--template-mask', help='Template brain mask', default="MNI152_T1_0.7mm_brain_mask.nii.gz")
     g.add_argument('--template-t1w-2mm', help='T1w 2mm template', default="MNI152_T1_2mm.nii.gz")
-    g.add_argument('--template-2mm-mask', help='2mm template brain mask', default="MNI152_T1_2mm_brain_mask_dil.nii.gz")
+    g.add_argument('--template-mask-2mm', help='2mm template brain mask', default="MNI152_T1_2mm_brain_mask_dil.nii.gz")
     g.add_argument('--template-t2w', help='T2w template', default="MNI152_T2_0.7mm.nii.gz")
     g.add_argument('--template-t2w-brain', help='T2w brain template', default="MNI152_T2_0.7mm_brain.nii.gz")
     g.add_argument('--template-t2w-2mm', help='T2w 2mm template', default="MNI152_T2_2mm.nii.gz")
@@ -117,20 +93,19 @@ def main():
     if not os.path.exists(args.subjlist):
         parser.error(f"Subject list file {args.subjlist} does not exist")
 
-    setup_logging(args.logfile, "debug" if args.debug else "info", log_stream=sys.stdout)
-
     # For string formatting convenient to have arguments as dictionary
     argdict = vars(args)
 
     # Start up pipeline and get key 
-    LOG.info(f"HCP Pipelines Nottingham wrapper v{__version__}")
-    LOG.info("Subject folders located in: {basedir}".format(argdict))
-    LOG.info("Subject list file: {subjlist}".format(argdict))
+    print(f"HCP Pipelines Nottingham wrapper v{__version__}")
+    print("Subject folders located in: {basedir}".format(**argdict))
+    print("Subject list file: {subjlist}".format(**argdict))
 
     argdict["fsldir"] = envdir("FSLDIR")
     argdict["hcpdir"] = envdir("HCPPIPEDIR")
     argdict["hcp_templ_dir"] = envdir("HCPPIPEDIR_Templates")
     argdict["hcp_config_dir"] = envdir("HCPPIPEDIR_Config")
+    print(argdict)
 
     # If SGE_ROOT is set, use long.q FIXME how does this relate to Nottingham setup?
     argdict["queue"] = ""
@@ -139,6 +114,7 @@ def main():
 
     # Apply standard prefix directories to templates and config dirs
     for k in argdict:
+        print(k)
         if k.startswith("template") and not os.path.isabs(argdict[k]):
             argdict[k] = os.path.join(argdict["hcp_templ_dir"], argdict[k])
         if k.startswith("config") and not os.path.isabs(argdict[k]):
@@ -150,31 +126,31 @@ def main():
             subjid = subjid.strip()
             subjdir = os.path.join(args.basedir, subjid)
             if not os.path.exists(subjdir):
-                LOG.warn("Subject directory not found for {subjid} - skipping")
+                print(f"WARNING: Subject directory not found for {subjid} - skipping")
                 continue
 
-            t1fname = args.t1w.format(subjid=subjid)
+            t1fname = os.path.join(subjdir, args.t1w.format(subjid=subjid))
             if not os.path.exists(t1fname):
-                LOG.warn("T1w image: {t1fname} not found for {subjid} - skipping")
+                print(f"WARNING: T1w image: {t1fname} not found for {subjid} - skipping")
                 continue
 
             if args.t2w:
-                t2fname = args.t2w.format(subjid=subjid)
+                t2fname = os.path.join(subjdir, args.t2w.format(subjid=subjid))
                 if not os.path.exists(t2fname):
-                    LOG.warn("T2w image: {t2fname} not found for {subjid} - skipping")
+                    print(f"WARNING: T2w image: {t2fname} not found for {subjid} - skipping")
                     continue
             else:
                 t2fname = "NONE"
 
             argdict["subjid"] = subjid
-            argdict["subjid"] = subjdir
+            argdict["subjdir"] = subjdir
             argdict["subjlogdir"] = os.path.join(subjdir, args.logdir)
             argdict["t1fname"] = t1fname
             argdict["t2fname"] = t1fname
             makedirs(argdict["subjlogdir"], exist_ok=True)
 
             if "PreFreeSurfer" not in args.skip:
-                # Run pre-freesurfer
+                print("\nSubmitting pre-freesurfer")
                 cmd = '{fsldir}/bin/fsl_sub {queue} \
                     -l {subjlogdir} \
                     {hcpdir}/PreFreeSurfer/PreFreeSurferPipeline.sh \
@@ -200,11 +176,11 @@ def main():
                     --t2samplespacing="{fmap_t2w_sample_spacing}" \
                     --unwarpdir="{fmap_unwarp_dir}" \
                     --gdcoeffs="{gdc_coeffs}" \
-                    --topupconfig="{topup_config}"'.format(argdict)
-                LOG.info(cmd)
+                    --topupconfig="{topup_config}"'.format(**argdict)
+                if args.debug: print(cmd)
 
             if "FreeSurfer" not in args.skip:
-                # Run freesurfer
+                print("\nSubmitting freesurfer")
                 # FIXME what if t2 does not exist?
                 cmd = '{fsldir}/bin/fsl_sub {queue} \
                     -l {subjlogdir} \
@@ -214,11 +190,11 @@ def main():
                     --subjectDIR="{subjdir}/T1w" \
                     --t1="{subjdir}/T1w/T1w_acpc_dc_restore.nii.gz" \
                     --t1brain="{subjdir}/T1w/T1w_acpc_dc_restore_brain.nii.gz" \
-                    --t2="{subjdir}/T1w/T2w_acpc_dc_restore.nii.gz"'.format(argdict)
-                LOG.info(cmd)
+                    --t2="{subjdir}/T1w/T2w_acpc_dc_restore.nii.gz"'.format(**argdict)
+                if args.debug: print(cmd)
       
             if "PostFreeSurfer" not in args.skip:
-                # Run post freesurfer
+                print("\nSubmitting post-freesurfer")
                 # FIXME what if t2 does not exist?
                 cmd = 'j{fsldir}/bin/fsl_sub {queue} \
                     -l {subjlogdir} \
@@ -233,8 +209,8 @@ def main():
                     --lowresmesh="{lores_mesh}" \
                     --subcortgraylabels="{config_subcort_gray_labels}" \
                     --freesurferlabels="{config_freesurfer_labels}" \
-                    --refmyelinmaps="{template_ref_myelin_maps}"'.format(argdict)
-                LOG.info(cmd)
+                    --refmyelinmaps="{template_ref_myelin_maps}"'.format(**argdict)
+                if args.debug: print(cmd)
       
 if __name__ == "__main__":
     main()
